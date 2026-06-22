607e36cc84e8bb72804aab4476a312bbbf5526cf
TY12346 <73483501+TY12346@users.noreply.github.com>
2026-06-22 17:11:10 +0800

    Add interview self scheduling backend

--- backend/apps/interviews/admin.py
+++ backend/apps/interviews/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import CalendarEvent, Interview, InterviewInvitation, InterviewStatusHistory
+from .models import CalendarEvent, Interview, InterviewInvitation, InterviewSchedulingRequest, InterviewStatusHistory, InterviewerAvailabilitySlot
 
 
 @admin.register(Interview)
@@ -40,6 +40,20 @@ class InterviewStatusHistoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'interview', 'from_status', 'to_status', 'changed_by', 'changed_at')
     list_filter = ('from_status', 'to_status')
     search_fields = ('interview__application__job__title', 'interview__application__applicant__email', 'changed_by__email', 'note')
+
+
+@admin.register(InterviewerAvailabilitySlot)
+class InterviewerAvailabilitySlotAdmin(admin.ModelAdmin):
+    list_display = ('id', 'organization', 'interviewer', 'start_datetime', 'end_datetime', 'status')
+    list_filter = ('status', 'organization')
+    search_fields = ('interviewer__email', 'interviewer__full_name')
+
+
+@admin.register(InterviewSchedulingRequest)
+class InterviewSchedulingRequestAdmin(admin.ModelAdmin):
+    list_display = ('id', 'application', 'organization', 'recruiter', 'interviewer', 'status', 'expires_at')
+    list_filter = ('status', 'organization')
+    search_fields = ('application__job__title', 'application__applicant__email', 'recruiter__email', 'interviewer__email', 'remark')
 
 
 @admin.register(CalendarEvent)
